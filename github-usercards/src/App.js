8bc8dd0d01fab1ea5fbd8a17530e8d2511b9f8c5
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2019-10-01 21:35:35 -0700

    have app functioning, just need to style it to get MVP

--- github-usercards/src/App.js
+++ github-usercards/src/App.js
@@ -1,42 +1,104 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Card from "./components/Card";
 import CardList from "./components/CardList";
-import fetchUser from "./functions/fetchUser";
+import axios from 'axios';
+
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
       handle: "",
-      user: null,
-      followers: []
+      user: {},
+      haveUser: false,
+      followers: [],
+      haveFollowers: false
     };
   }
 
-  handleChange = e => {
-    this.setState({ handle: e.target.value });
+  handleChanges = e => {
+    this.setState({
+      handle: e.target.value
+    });
   }
 
-  handleSubmit = e => {
+  fetchUser = e => {
     e.preventDefault();
-    this.setState({
-      user: fetchUser(`https://api.github.com/users/${this.state.handle}`)
-    });
+    console.log(this.state.handle);
+    axios
+      .get(`https://api.github.com/users/${this.state.handle}`)
+      .then(res => {
+        console.log(res);
+        this.setState({
+          user: res.data
+        });
+      })
+      .catch(err => console.log("Error getting user information:\n", err));
+
+  }
+  fetchFollowers = e => {
+    e.preventDefault();
+    axios
+      .get(`https://api.github.com/users/${this.state.handle}/followers`)
+      .then(res => {
+        console.log(res);
+        this.setState({
+          followers: res.data,
+          haveFollowers: true
+        });
+      })
+      .catch(err => console.log("Error getting followers:\n", err));
+  }
+
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.user !== this.state.user) {
+      this.setState({
+        haveUser: true
+      });
+    }
   }
 
   render() {
     return (
-
       <div className="App">
-        <form onSubmit={this.handleSubmit}>
-          <input type="text" value={this.state.user} onChange={this.handleChange} placeholder="Enter GitHub handle." />
-          <button type="submit">Get User</button>
+        <h1>GitHub User Cards</h1>
+        <form onSubmit={this.fetchUser}>
+          <input
+            type="text"
+            value={this.state.handle}
+            placeholder="Enter user's GitHub handle."
+            onChange={this.handleChanges} />
+          <input type="submit" value="Get User" />
         </form>
-        <CardList users={this.state.followers} />
+        {
+          this.state.haveUser
+            ? (
+              <>
+                <h2>User</h2>
+                <Card key={this.state.user.id} user={this.state.user} />
+                <button onClick={this.fetchFollowers}>See Followers</button>
+              </>
+            )
+            : <></>
+        }
+
+        {
+          this.state.haveFollowers
+            ? (
+              <>
+                <h2>Followers</h2>
+                {this.state.followers.map(follower => <Card key={follower.id} user={follower} />)}
+              </>
+            )
+            : <></>
+        }
       </div>
     );
   }
+
 }
 
 export default App;
--- github-usercards/src/components/Card.js
+++ github-usercards/src/components/Card.js
@@ -1,10 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import axios from "axios";
 
-class Card extends React.Component {
-    render() {
-        return;
-    }
+const Card = user => {
+    console.log("user.login passed to Card:\n", user.user);
+    return (
+        <div className="card">
+            <img
+                alt={`${user.user.login}'s profile picture`}
+                src={user.user.avatar_url}
+            />
+            <h3>{user.user.login}</h3>
+            <p>URL: {user.user.html_url}</p>
+
+        </div>
+    );
 }
 
 export default Card;
